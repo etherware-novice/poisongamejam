044d1c2a73ec81cf780caff56fca9d57341d3435
etherware-novice <candy@notarealaddr.com>
2023-04-02 01:27:52 -0500

    work clock has art now

--- scripts/workclock.gd
+++ scripts/workclock.gd
@@ -1,12 +1,12 @@
 extends AnimatedSprite2D
 
-var hoursLeft = 12  # 120 seconds (2 minutes)
+var hoursLeft = 12  # 120 seconds (2 mins)
 
 signal timeUp
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	play(str(hoursLeft))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,6 +16,6 @@ func _process(delta):
 
 func _on_hour_tick_timeout():
 	hoursLeft -= 1
-	scale.x = hoursLeft
-	if hoursLeft < 0:
+	animation = str(hoursLeft)
+	if hoursLeft < 1:
 		timeUp.emit()
